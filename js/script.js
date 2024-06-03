9c4f87122b846772b282d50b746293d1ef18a704
bayu <bayuharmo55@gmail.com>
2024-06-04 06:53:34 +0700

    inisiasi config

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-//  Ini Javascript
+alert('Halo Selamat Datang di Website Airlangga Digital')
+
+function cetak() {
+    var a = document.getElementById("full-name").value ; 
+    document.getElementById("hasil-nama").innerHTML = "<b>" + a + "</b>" ;
+
+    var b= document.getElementById("birt-date").value ; 
+    document.getElementById("hasil-ttl").innerHTML = "<b>" + b + "</b>" ;
+
+    var w= document.getElementById("tanggal").value ; 
+    document.getElementById("hasil-tgl").innerHTML = "<b>" + w + "</b>" ;
+
+    var c = document.getElementById("kebutuhan").value ; 
+    document.getElementById("hasil-kebutuhan").innerHTML = "<b>" + c + "</b>" ;
+
+    var d = document.getElementById("Keterangan").value ; 
+    document.getElementById("hasil-keterangan").innerHTML = "<b>" + d + "</b>" ;
+
+    var e = document.getElementById("nomor").value ; 
+    document.getElementById("hasil-nomor").innerHTML = "<b>" + e + "</b>" ;
+
+}
